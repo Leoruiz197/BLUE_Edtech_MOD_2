dd1f61ddbb8f2e7a70732b5f41ca24bea5963b84
Leoruiz197 <ruizruizleo@hotmail>
2021-09-24 22:55:52 -0300

    js final da aula comentado

--- Aula_05/JS/script.js
+++ Aula_05/JS/script.js
@@ -3,21 +3,21 @@ function teste(){
 }
 
 function nome(){
-    const nome = document.getElementById("inputnome").value;
+    const nome = document.getElementById("inputnome").value; /* getElementById  recupera um elemento por completo, com o .value ele traz apenas o valor */
     document.getElementById("inputnome").value = "";
     
     if(nome == ""){
-        alert("preencha primeiro o campo nome");
+        alert("preencha primeiro o campo nome"); /* aparece na tela um balao com o escrito no meio */
     }else{
         
-        document.getElementById("spanresultado").textContent = nome;
-        document.getElementById("spanresultado").style.color = "red";
+        document.getElementById("spanresultado").textContent = nome; /* textcontent insere o texto no meio das tags html <span> INSERE AQUI </span>*/
+        document.getElementById("spanresultado").style.color = "red"; /* .style modifica o CSS atraves do JS */
     }
 
     // alert(nome);
 }
 
-function sec1(){
+function sec1(){ /* toogle */
     var sec = document.getElementById("primeiro");
 
     if(sec.style.display === "none"){
@@ -42,7 +42,30 @@ function calcularIMC(){
     var peso = document.getElementById("peso").value;
     var altura = document.getElementById("altura").value;
 
-    var resultado = peso / (altura * altura)
+    var resultado = peso / (altura * altura); /* calculos matematicos com o valor retornado dos inputs */
 
-    alert(resultado);
+    document.getElementById("spanIMC").textContent = resultado.toFixed(4); /* .toFixed(valor)  serve para arredondar as casas decimais */
+    //alert(resultado.toFixed(2));
+}
+
+function addTarefa(){
+    const li = document.createElement("li"); /* criar uma tag <li></li> HTML no DOM*/
+    const tarefa = document.getElementById("inputtarefa").value;
+    document.getElementById("inputtarefa").value = "";
+
+    if(tarefa == ""){
+        alert("Preencha o campo de tarefas antes de adicionar!");
+    }else{
+        const textotarefa = document.createTextNode(tarefa); /* Cria o texto preparado para ser inserido em uma tag HTML nova */
+        li.appendChild(textotarefa); /* add filho ao pai, add conteudo ou outra tag */
+
+        document.getElementById("MinhaLista").appendChild(li);
+    }
+}
+
+function create(){
+    const titulo2 = document.createElement("h2");
+    const textoh2 = document.createTextNode("titulo h2");
+    titulo2.appendChild(textoh2);
+    document.getElementById("quarto").appendChild(titulo2);
 }
